68a7f9ad6203cd77b0c5ae70ba65bbd6f2433570
Ken Orji <kokenjr@gmail.com>
2015-12-01 15:04:04 -0500

    updated bash script

--- lib/commander.rb
+++ lib/commander.rb
@@ -1,6 +1,12 @@
 module Commander
   module_function
+    # HOST = '10.1.10.60'
+    # USER = 'makebuild'
+    # PASS = ''
     def random
-      system 'sh /home/makebuild/scripts/random.sh'
+      system 'bash /home/makebuild/scripts/random.sh'
+      # Net::SSH.start( HOST, USER, :password => PASS ) do| ssh |
+      #    ssh.exec "sh /home/makebuild/scripts/random.sh"
+      # end
     end
 end
