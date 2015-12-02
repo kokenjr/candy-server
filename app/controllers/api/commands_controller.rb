9d87a4eff350bd49cce1cc6c08e9d97fb7699df5
Ken Orji <kokenjr@gmail.com>
2015-12-01 19:09:59 -0500

    strip space in commands

--- app/controllers/api/commands_controller.rb
+++ app/controllers/api/commands_controller.rb
@@ -2,12 +2,12 @@ require 'commander'
 class Api::CommandsController < Api::BaseController
   def random
     puts "request random command"
-    Commander.random(params[:audio])
+    Commander.random(params[:audio].strip)
     render json: {message: "success"}
   end
   def say
     puts "request say command"
-    Commander.say(params[:text])
+    Commander.say(params[:text].strip)
     render json: {message: "success"}
   end
 end
