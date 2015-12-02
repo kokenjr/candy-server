5af48c76d7be186f2966d1e1827b915291f94505
Ken Orji <kokenjr@gmail.com>
2015-12-02 16:26:23 -0500

    modified downcase

--- app/controllers/api/commands_controller.rb
+++ app/controllers/api/commands_controller.rb
@@ -1,15 +1,15 @@
 require 'commander'
 class Api::CommandsController < Api::BaseController
   def random
-    Commander.random(params[:audio].strip)
+    Commander.random(params[:audio].strip.downcase)
     render json: {message: "success"}
   end
   def say
-    Commander.say(params[:text].strip)
+    Commander.say(params[:text].strip.downcase)
     render json: {message: "success"}
   end
   def status
-    Commander.status(params[:state])
+    Commander.status(params[:state].downcase)
     render json: {message: "success"}
   end
 end
