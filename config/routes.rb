3f5eef3b3468be13d0558bf318c34f87274e371d
Ken Orji <kokenjr@gmail.com>
2015-12-01 19:18:06 -0500

    added routes for speak command

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     get 'commands/random' => 'commands#random'
+    post 'commands/say' => 'commands#say'
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
