4c069aefd648f7676ac92d70c66812874d879a40
Ken Orji <kokenjr@gmail.com>
2015-12-04 13:23:52 -0500

    fixed bash bug

--- script/random.sh
+++ script/random.sh
@@ -6,7 +6,7 @@ choice=
 IFS=$'\n'
 # SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SCRIPT_PATH="$( dirname "${BASH_SOURCE[0]}" )"
-dir="$SCRIPT_PATH/../public/sounds$2"
+dir="$SCRIPT_PATH/../public/sounds/$2"
 Files=`find $dir -type f`
 
 file=($Files)                # Read into array variable.
