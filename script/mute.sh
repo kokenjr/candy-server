a106702631f117885cf58d335dad0ff57afd69e7
Ken Orji <kokenjr@gmail.com>
2015-12-04 15:13:32 -0500

    added change to run blather

--- script/mute.sh
+++ script/mute.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_PATH="$( dirname "${BASH_SOURCE[0]}" )"
-$SCRIPT_PATH/say.sh "bye, daddy"
+$SCRIPT_PATH/say.sh en "bye, daddy"
 
 amixer -D pulse set Master 0%
 
--- script/wake.sh
+++ script/wake.sh
@@ -2,6 +2,6 @@
 SCRIPT_PATH="$( dirname "${BASH_SOURCE[0]}" )"
 amixer -D pulse set Master 70%
 
-$SCRIPT_PATH/say.sh "dollar dollar bill y'all" 
+$SCRIPT_PATH/say.sh en "dollar dollar bill y'all" 
 
 exit
